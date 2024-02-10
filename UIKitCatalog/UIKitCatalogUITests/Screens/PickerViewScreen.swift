4e15f0cba222144e05088479703481b9542337df
Vinicius Fagundes <vinipeixotofagundes@gmail.com>
2024-02-10 12:06:45 +0100

    xcuitest - picker wheel screen

---
+++ UIKitCatalog/UIKitCatalogUITests/Screens/PickerViewScreen.swift
@@ -0,0 +1,9 @@
+//
+//  PickerViewScreen.swift
+//  UIKitCatalogUITests
+//
+//  Created by Vinicius Fagundes on 08/02/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
---
+++ UIKitCatalog/UIKitCatalogUITests/Tests/PickerWheelScreenTests.swift
@@ -0,0 +1,9 @@
+//
+//  PickerWheelScreenTests.swift
+//  UIKitCatalogUITests
+//
+//  Created by Vinicius Fagundes on 08/02/24.
+//  Copyright © 2024 Apple. All rights reserved.
+//
+
+import Foundation
